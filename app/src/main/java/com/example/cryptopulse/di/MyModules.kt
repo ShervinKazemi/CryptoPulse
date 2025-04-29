f7f49336dba75110f9cd5e4aa96c6e152276c318
Shervin <github.kazmy2@gmail.com>
2025-04-29 10:13:47 +0330

    Add OnboardingViewModel

--- app/src/main/java/com/example/cryptopulse/di/MyModules.kt
+++ app/src/main/java/com/example/cryptopulse/di/MyModules.kt
@@ -1,5 +1,13 @@
 package com.example.cryptopulse.di
 
+import com.example.cryptopulse.model.repositories.onboarding.OnboardingRepository
+import com.example.cryptopulse.model.repositories.onboarding.OnboardingRepositoryImpl
+import com.example.cryptopulse.ui.presentation.onboarding.OnboardingViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val myModules = module {}
+val myModules = module {
+    single<OnboardingRepository> { OnboardingRepositoryImpl() }
+
+    viewModel { OnboardingViewModel(get()) }
+}
---
+++ app/src/main/java/com/example/cryptopulse/ui/presentation/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.cryptopulse.ui.presentation.onboarding
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptopulse.model.data.OnboardingData
+import com.example.cryptopulse.model.repositories.onboarding.OnboardingRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class OnboardingViewModel(private val onboardingRepository: OnboardingRepository) : ViewModel() {
+
+    private val _onboardingData = MutableStateFlow<List<OnboardingData>>(emptyList())
+    val onboardingData: StateFlow<List<OnboardingData>> = _onboardingData
+
+    init {
+        fetchOnboardingData()
+    }
+
+    private fun fetchOnboardingData() {
+        viewModelScope.launch {
+            val data = onboardingRepository.getOnboardingData()
+            _onboardingData.value = data
+        }
+    }
+}

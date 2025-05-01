9131cf894dd4c43b42bcf62383661b422d4016fc
Shervin <github.kazmy2@gmail.com>
2025-05-01 22:09:56 +0330

    Add getCoinsList Function for request coins from coinGecko

--- app/src/main/java/com/example/cryptopulse/model/net/ApiService.kt
+++ app/src/main/java/com/example/cryptopulse/model/net/ApiService.kt
@@ -1,26 +1,51 @@
 package com.example.cryptopulse.model.net
 
+import com.example.cryptopulse.model.data.CoinsData
 import com.example.cryptopulse.model.data.TrendingData
 import com.example.cryptopulse.util.Constants
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface ApiService {
 
-    @Headers(Constants.API_KEY)
     @GET("search/trending")
-    suspend fun getTrendingCoins() :Response<TrendingData>
+    suspend fun getTrendingCoins(): Response<TrendingData>
 
+    @GET("coins/markets")
+    suspend fun getCoinsList(
+        @Query("vs_currency") vsCurrency: String = "usd",
+        @Query("page") page: Int? = null,
+        @Query("per_page") perPage: Int? = null
+    ): Response<CoinsData>
 
-}
+    companion object {
+        private val retrofit: Retrofit by lazy {
+            val client = OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val request = chain.request().newBuilder()
+                        .addHeader("X-API-KEY", Constants.API_KEY)
+                        .build()
+                    chain.proceed(request)
+                }
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build()
 
-fun createApiService() :ApiService {
-    return Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(ApiService::class.java)
+            Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        fun create(): ApiService {
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 }

97437df26fe06054a418aae474a6a7ead35f5526
Shervin <github.kazmy2@gmail.com>
2025-05-02 09:39:30 +0330

    Add getCoinsList function successfully

--- app/src/main/java/com/example/cryptopulse/model/repositories/home/HomeRepository.kt
+++ app/src/main/java/com/example/cryptopulse/model/repositories/home/HomeRepository.kt
@@ -1,9 +1,12 @@
 package com.example.cryptopulse.model.repositories.home
 
+import com.example.cryptopulse.model.data.CoinsData
 import com.example.cryptopulse.model.data.TrendingData
+import kotlinx.coroutines.flow.Flow
 
 interface HomeRepository {
 
-    suspend fun getTrendingCoins() :TrendingData
+    suspend fun getTrendingCoins(): Flow<TrendingData>
 
+    suspend fun getCoinsList(): Flow<CoinsData>
 }
--- app/src/main/java/com/example/cryptopulse/model/repositories/home/HomeRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptopulse/model/repositories/home/HomeRepositoryImpl.kt
@@ -1,21 +1,36 @@
 package com.example.cryptopulse.model.repositories.home
 
+import com.example.cryptopulse.model.data.CoinsData
 import com.example.cryptopulse.model.data.TrendingData
 import com.example.cryptopulse.model.net.ApiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
 import java.io.IOException
 
+
 class HomeRepositoryImpl(private val apiService: ApiService) : HomeRepository {
-    override suspend fun getTrendingCoins(): TrendingData {
-        val response = apiService.getTrendingCoins()
+
+    override suspend fun getTrendingCoins(): Flow<TrendingData> {
+        return flow {
+            emit(handleResponse { apiService.getTrendingCoins() })
+        }
+    }
+
+    override suspend fun getCoinsList(): Flow<CoinsData> {
+        return flow {
+            emit(handleResponse { apiService.getCoinsList() })
+        }
+    }
+
+    private inline fun <T> handleResponse(call: () -> Response<T>): T {
+        val response = call()
         if (response.isSuccessful) {
-            val trendingResponse = response.body()
-            if (trendingResponse != null) {
-                return trendingResponse
-            } else {
-                throw IOException("Empty data body")
-            }
+            val body = response.body()
+                ?: throw IOException("Empty response body")
+            return body
         } else {
-            throw IOException("Api error : " + response.code().toString())
+            throw IOException("API error: ${response.code()} - ${response.message()}")
         }
     }
 }
